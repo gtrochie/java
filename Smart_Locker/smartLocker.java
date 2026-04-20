7635a0b2b076ae6245562b5f2350610b766a24b6
Rotshidzwa Nematswerani <gtrochie@gmail.com>
2026-04-20 11:19:09 +0200

    added the constructor and methods for smartLocker

--- Smart_Locker/smartLocker.java
+++ Smart_Locker/smartLocker.java
@@ -1,2 +1,30 @@
 public class smartLocker {
+    private boolean isOpen;
+    private int pin;
+
+    public smartLocker(){
+        this.isOpen = false;
+        this.pin = 1234;
+
+    }
+    public void unlock (int enteredpin){
+        if (pin == enteredpin){
+            isOpen = true;
+            System.out.println("the locker is opened");
+        }
+        else{
+            System.out.println("wrong pin");
+        }
+    }
+    public void lock(){
+        isOpen = false;
+    }
+    public void changePin(int oldPin, int newPin) {
+        if (this.pin == oldPin) {
+            this.pin = newPin;
+        }
+
+
+    }
 }
+

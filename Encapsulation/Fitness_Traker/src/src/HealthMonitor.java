e527c6ca78cf788afd8219daf6839961f5ab04bb
Rotshidzwa Nematswerani <gtrochie@gmail.com>
2026-04-21 11:38:41 +0200

    done with Healthmonitor

--- Encapsulation/Fitness_Traker/src/src/HealthMonitor.java
+++ Encapsulation/Fitness_Traker/src/src/HealthMonitor.java
@@ -9,5 +9,51 @@ public class HealthMonitor {
         this.isExercising = false;
 
     }
+    public void addSteps (int newsteps){
+        if (newsteps < 0){
+            System.out.println("you cannot have negative steps");
+        }
+        else {
+            steps += newsteps;
+        }
+    }
+    public void updateHeartRate(int currentHR){
+        this.heartRate = currentHR;
+        if (this.heartRate > 180){
+            this.isExercising = false;
+            System.out.println("DANGER: Slow down!");
+        }
+        else if (heartRate > 100){
+            this.isExercising = true;
+
+        }
+        else {
+            this.isExercising = false;
+        }
+    }
+
+    public String getStatus(){
+        if (heartRate < 100){
+          this.steps = 0;
+          return "Resting";
+        }
+        else if(heartRate > 180){
+            return "DANGER!!,slow down";
+        }
+        else {
+            return "exercising";
+        }
+    }
+    public void resetDailyStats(){
+        if (heartRate < 100){
+            this.steps = 0;
+
+        }
+    }
+
+    public  String getSummary(){
+        return "Steps: " + steps + " | HR: " + heartRate + " | Status: " + getStatus();
+    }
+
 
 }

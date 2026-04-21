68ca7e5e98d627a483df8aba3621ef898b902afa
Rotshidzwa Nematswerani <gtrochie@gmail.com>
2026-04-21 11:55:32 +0200

    done addWater Method

--- Encapsulation/Smart_coffee_Machine/src/src/CoffeeMachine.java
+++ Encapsulation/Smart_coffee_Machine/src/src/CoffeeMachine.java
@@ -1,2 +1,22 @@
 public class CoffeeMachine {
+    private int waterLevel;
+    private int beanWeight;
+    private int totalBrewedCount;
+
+    public CoffeeMachine(){
+        this.waterLevel = 0;
+        this.beanWeight = 0;
+        this.totalBrewedCount = 0;
+
+    }
+
+    public void addWater(int ml){
+        if (ml > 1000){
+            waterLevel = 1000;
+        }
+        else {
+            waterLevel += ml;
+        }
+    }
+
 }

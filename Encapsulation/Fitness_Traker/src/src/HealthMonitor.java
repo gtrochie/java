33313b3192b388d09d410f73948bdc60c2ac19e2
Rotshidzwa Nematswerani <gtrochie@gmail.com>
2026-04-21 10:16:36 +0200

    done with constructor

--- Encapsulation/Fitness_Traker/src/src/HealthMonitor.java
+++ Encapsulation/Fitness_Traker/src/src/HealthMonitor.java
@@ -1,2 +1,13 @@
 public class HealthMonitor {
+    private int steps;
+    private int heartRate;
+    private boolean isExercising;
+
+    public HealthMonitor(){
+        this.steps = 0;
+        this.heartRate = 0;
+        this.isExercising = false;
+
+    }
+
 }

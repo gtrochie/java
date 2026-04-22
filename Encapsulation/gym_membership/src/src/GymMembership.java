995601b994b132498d3fdf8ec466d20047ad1f83
Rotshidzwa Nematswerani <gtrochie@gmail.com>
2026-04-22 14:35:36 +0200

    done with the project

--- Encapsulation/gym_membership/src/src/GymMembership.java
+++ Encapsulation/gym_membership/src/src/GymMembership.java
@@ -1,2 +1,31 @@
 public class GymMembership {
+    private String memberName;
+    private String planType;
+    private int monthlyFee;
+
+    public GymMembership(String memberName){
+        this.memberName = memberName;
+        this.planType = "Basic";
+
+    }
+    public  GymMembership (String memberName, String requestedPlan){
+        this.memberName = memberName;
+        if (requestedPlan.equalsIgnoreCase("Gold")){
+            this.planType = "Gold";
+            this.monthlyFee = 500;
+        }
+        else {
+            this.planType = "Basic";
+            this.monthlyFee = 200;
+            System.out.println(memberName + " has upgraded to Gold");
+        }
+    }
+    public String getSummary(){
+        // "Member: [Name] | Plan: [Plan] | Fee: R[Amount]"
+        return "Member " + memberName +  " | Plan: " + planType + " | Fee: R" + monthlyFee;
+    }
+    public void upgradeToGold(){
+        this.planType = "Gold";
+        this.monthlyFee = 500;
+    }
 }
--- Encapsulation/gym_membership/src/src/GymTest.java
+++ Encapsulation/gym_membership/src/src/GymTest.java
@@ -1,2 +1,10 @@
 public class GymTest {
+    public static void main(String[] args) {
+        GymMembership member1 = new GymMembership("Rochie");
+        GymMembership member2 = new GymMembership("Steven", "gold");
+
+        System.out.println(member1.getSummary());
+        member1.upgradeToGold();
+        System.out.println( member2.getSummary());
+    }
 }

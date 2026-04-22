9e66139f5f8220593dfde9358af2371e8379c5aa
Rotshidzwa Nematswerani <gtrochie@gmail.com>
2026-04-22 13:33:39 +0200

    done but still to test

--- Encapsulation/bank_account/BankAccount.java
+++ Encapsulation/bank_account/BankAccount.java
@@ -1,2 +1,40 @@
 public class BankAccount {
+    private String accountHolder;
+    private int balance;
+
+    public BankAccount(String accountHolder){
+        this.accountHolder = accountHolder;
+        this.balance = 0;
+    }
+    public BankAccount(String accountHolder, int balance){
+        this.accountHolder = accountHolder;
+        if (balance > 0){
+            this.balance = balance;
+        }
+        else {
+            this.balance = 0;
+        }
+    }
+    public void deposit (int amount){
+        if (amount > 0){
+            balance += amount;
+        }
+        else {
+            System.out.println("error : cannot deposit a negative amount");
+        }
+    }
+
+    public void withdraw(int amount){
+        if (amount > 0 && amount <= balance){
+            balance -= amount;
+            System.out.println("Withdrew: R" + amount);
+        }
+        else{
+            System.out.println("BLOCKED: insufficient funds");
+        }
+    }
+
+    public int getbalance(){
+        return balance;
+    }
 }
--- Encapsulation/bank_account/BankTest.java
+++ Encapsulation/bank_account/BankTest.java
@@ -1,2 +1,8 @@
 public class BankTest {
+    BankAccount account1 = new BankAccount("Rochie", 2000000000);
+    BankAccount account2 = new BankAccount("scott");
+    BankAccount  account3 = new BankAccount("Lolly",-2500);
+
+    account1.withdraw(50000);
+
 }

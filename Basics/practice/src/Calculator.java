9b595b3c718e8b6b9a840e47909d6a3cf61e4184
Rotshidzwa Nematswerani <gtrochie@gmail.com>
2026-04-23 15:17:17 +0200

    file name change

--- Basics/practice/src/Calculator.java
+++ Basics/practice/src/Calculator.java
@@ -1,6 +1,6 @@
 import  java.util.Scanner;
 
-public class Practice {
+public class Calculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("enter the first number: ");

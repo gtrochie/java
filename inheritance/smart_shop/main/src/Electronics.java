7fbcf12793e8d268f2b0265cbf51a3b93bfa9c42
Rotshidzwa Nematswerani <gtrochie@gmail.com>
2026-04-23 15:33:16 +0200

    electronics

--- inheritance/smart_shop/main/src/Electronics.java
+++ inheritance/smart_shop/main/src/Electronics.java
@@ -1,2 +1,14 @@
-public class Electronics {
+
+public class Electronics extends Product {
+    private int warrantyMonths;
+
+    public Electronics(String name, double price, int warrantyMonths) {
+
+        super(name, price);
+        this.warrantyMonths = warrantyMonths;
+    }
+
+    public void printWarranty() {
+        System.out.println("This item has a " + warrantyMonths + "-month warranty.");
+    }
 }
